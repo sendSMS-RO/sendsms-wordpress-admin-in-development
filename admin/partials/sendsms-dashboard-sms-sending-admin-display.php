d530ad0c2cc3232bea63ba8e4f79ece3cce66ef0
Temepest74 <raducata11@yahoo.com>
2021-07-20 11:58:47 +0300

    tooltip not showing bug fix

--- admin/partials/sendsms-dashboard-sms-sending-admin-display.php
+++ admin/partials/sendsms-dashboard-sms-sending-admin-display.php
@@ -6,23 +6,37 @@
         <div class="sendsms-container-grid-test">
             <div class="sendsms-item-input-1">
                 <span id="forPhoneNumber"><?php echo __('Phone number', 'sendsms-dashboard') ?></span>
-                <i class="fas fa-question-circle tooltip" data-title="<?php echo __('We recommend a phone number in E.164 Format but without the + sign', 'sendsms-dashboard') ?>"></i>
+                <div class="tooltip">
+                    <i class="fas fa-question-circle"></i>
+                    <span class="tooltiptext">
+                        <?php echo __('We recommend a phone number in E.164 Format but without the + sign', 'sendsms-dashboard') ?>
+                    </span>
+                </div>
             </div>
             <input id="phone_number" type="tel" class="sendsms-item-input-1" placeholder="40727363767" aria-label="Phone number" aria-describedby="forPhoneNumber">
             <div class="sendsms-item-input-1">
                 <span id="forGdpr"><?php echo __('Add unsubscribe link?', 'sendsms-dashboard') ?></span>
-                <i class="fas fa-question-circle tooltip" data-title="<?php echo __('You must specify {gdpr} key message. {gdpr} key will be replaced automatically with confirmation unique confirmation link. If {gdpr} key is not specified confirmation link will be placed at the end of message.', 'sendsms-dashboard') ?>"></i>
+                <div class="tooltip">
+                    <i class="fas fa-question-circle"></i>
+                    <span class="tooltiptext">
+                        <?php echo __('You must specify {gdpr} key message. {gdpr} key will be replaced automatically with confirmation unique confirmation link. If {gdpr} key is not specified confirmation link will be placed at the end of message.', 'sendsms-dashboard') ?>
+                    </span>
+                </div>
             </div>
             <input id="gdpr" class="sendsms-item-input-1" type="checkbox" value="gdpr" aria-label="Unsubscribe Link" aria-describedby="forGdpr">
             <div class="sendsms-item-input-1">
                 <span id="forShort"><?php echo __('Shrink urls?', 'sendsms-dashboard') ?></span>
-                <i class="fas fa-question-circle tooltip" data-title="<?php echo __('Searches long url and replaces them with corresponding short url. Please use only urls that start with https:// or http://', 'sendsms-dashboard') ?>"></i>
+                <div class="tooltip">
+                    <i class="fas fa-question-circle"></i>
+                    <span class="tooltiptext">
+                        <?php echo __('Searches long url and replaces them with corresponding short url. Please use only urls that start with https:// or http://', 'sendsms-dashboard') ?>
+                    </span>
+                </div>
             </div>
             <input id="short" class="sendsms-item-input-1" type="checkbox" value="short" aria-label="Shrink urls?" aria-describedby="forShort">
 
             <div class="sendsms-item-input-1">
                 <span id="forMessage"><?php echo __('Message', 'sendsms-dashboard') ?></span>
-                <i class="fas fa-question-circle tooltip" data-title="<?php echo __('The message you want to send', 'sendsms-dashboard') ?>"></i>
             </div>
             <textarea rows="4" id="message" class="sendsms-item-input-1 sendsms_dashboard_content" aria-label="Message" aria-describedby="forMessage" data-sendsms-counter="counterMessage"></textarea>
             <p id="counterMessage" class="sendsms-item-input-2"><?php echo __("The field is empty", 'wc_sendsms') ?></p>
